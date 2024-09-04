c523126f1b0fcd21e8d6f361eec16c9345b9fa8e
VicoAritonang <chaikaaaaa5@gmail.com>
2024-09-04 14:21:50 +0700

    selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jlc$942-wpow&5ib@u54yb+n*d)*3n8r^5lcj-3*#mx7+ih$74
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "vico-winner31-mentalhealthtracker.pbp.cs.ui.ac.id"]
-
+    
 
 # Application definition
 
