3c73caea9606aad8a3b175829cffa4e0fade255d
VicoAritonang <chaikaaaaa5@gmail.com>
2024-09-18 14:29:26 +0700

    ayolah

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import login_user,logout_user
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register
 
 app_name = 'main'
-
+# tes
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
